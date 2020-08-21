c77c5d5fe1f16c32a84ad24d062294caf1aca73f
BenNasrBasma <bennasrbasma97@gmail.com>
2020-08-22 01:22:09 +0200

    creating page two,hide the inputs

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-function goLogin(){
+$(document).ready(function(){
+/*function goLogin(){
 	$('#signup').hide()
 	$('#signin').show()
 }
@@ -6,3 +7,15 @@ function goRegister(){
 	$('#signup').show()
 	$('#signin').hide()
 }
+*/
+
+$('btn').click(function(){
+	if($("#password").val()==="12345"&& $("#account").val()==="admin"){
+		console.log("access accepted")
+		window.location.replace("website bank.html");	
+	}else{
+		$("#msgerr").show();
+	}
+});
+});  
+
