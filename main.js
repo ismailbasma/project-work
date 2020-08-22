60f3f7b245ae90394d5fda3eea4d8ba6a8271725
BenNasrBasma <bennasrbasma97@gmail.com>
2020-08-22 08:38:54 +0200

    adding the logo,margin and padding the function that will hide and show the messages

--- main.js
+++ main.js
@@ -1,10 +1,44 @@
+var users = [];
 
-/*want to hide the inputs and when i click on the sub button it will show*/
-$(document).ready(function(){
-	('#btn1').click(function(){
-		$("div").hide();
-	});
-	$("#btn1").click(function(){
-		$("div").show();
-	});
-});
+
+
+
+$('#gologin').on('click',function(){
+	$('#loginpage').show()
+	$('#registerpage').hide()
+})
+
+$('#goregister').on('click',function(){
+	$('#loginpage').hide()
+	$('#registerpage').show()
+	
+})
+
+
+$("#signUp").on('click', function(){
+	var name = $('#user').val();
+	var email = $('#em').val();
+	var password = $('#pass').val();
+    users.push()
+    });
+
+
+
+// $('#user').val()===user['Basma Ben Nasr'];
+// $('#em').val()===user['basmaben.97@gmail.com'];
+// $('#pass').val()===user['basma123'];
+
+//$('.btn')on('click',function(){
+
+
+//})
+
+/*
+
+var user = {
+	username : '',
+	email : '',
+	password : ''
+}
+
+*/
